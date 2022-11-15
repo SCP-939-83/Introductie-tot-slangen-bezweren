f63c9be3057d0bb5e93577c2ff52299a162d0c64
SCP-939-83 <512707@vistacollege.nl>
2022-11-15 11:20:20 +0100

    Fibonacci sequence

---
+++ Fibonacci sequence/fibonacci.py
@@ -0,0 +1,27 @@
+#function aangemaakt
+def Fibonacci(length):
+
+    first = 0
+    second = 1
+
+    print(first, second, end=" ")
+
+#maakt het een kleinere loop
+    length -= 2
+    
+#voor loop
+    while length > 0:
+
+
+        print(first + second, end=" ")
+ #replace oude value's met nieuwe
+        temp = second
+        second = first + second
+        first = temp
+
+
+        length -= 1
+
+if __name__ :
+    Fibonacci(50)
+
